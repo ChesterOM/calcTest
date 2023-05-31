0ab19f2de206bf564220b418acfa4d1f6906bfe7
ChesterOM <131749381+ChesterOM@users.noreply.github.com>
2023-05-31 20:11:56 +1200

    Add HTML and CSS for calculator UI

--- java.js
+++ java.js
@@ -1,65 +1,65 @@
-let userNum1 
-let userNum2 
-let userOperator
-let total = 0
+let userNum1;
+let userNum2; 
+let userOperator;
+let total = 0;
 
 function toNumber(){
     if (userNum1 || userNum2 == typeof 'string'){
         userNum1 = Number(userNum1); 
         userNum2 = Number(userNum2);
     }
-}
+};
 
 function add(){
-    total = userNum1 + userNum2
-    console.log(total)
-    return total
-}
+    total = userNum1 + userNum2;
+    console.log(total);
+    return total;
+};
 
 function sub(){
-    total = userNum1 - userNum2
-    console.log(total)
-    return total
-}
+    total = userNum1 - userNum2;
+    console.log(total);
+    return total;
+};
 
 function multi(){
-    total = userNum1 * userNum2
-    console.log(total)
-    return total
-}
+    total = userNum1 * userNum2;
+    console.log(total);
+    return total;
+};
 
 function divide(){
-    total = userNum1 / userNum2
-    console.log(total)
-    return total
-}
+    total = userNum1 / userNum2;
+    console.log(total);
+    return total;
+};
 
 function operate(userOperator){
     switch(userOperator){
         case '+':
-            total = add()
+            total = add();
             break;
         case '-':
-            total = sub()
+            total = sub();
             break;
         case '*':
-            total = multi()
+            total = multi();
             break;
         case '/':
-            total = divide()
+            total = divide();
             break;
     }
 }
 
 function testInput(){
-     userNum1 = prompt()
-     userOperator = prompt()
-     userNum2 = prompt()
+     userNum1 = prompt();
+     userOperator = prompt();
+     userNum2 = prompt();
     
-    toNumber()
-    operate(userOperator)
-}
+    toNumber();
+    operate(userOperator);
+};
 
-const testbtn = document.getElementById('btn')
-testbtn.addEventListener('click', testInput)
+const testbtn = document.getElementById('btn');
+testbtn.addEventListener('click', testInput);
 
