498c1f34e114e24d703222ce9f82df0bba8b634c
ChesterOM <131749381+ChesterOM@users.noreply.github.com>
2023-05-31 19:05:13 +1200

    Add functions to complete basic calculations through console

---
+++ java.js
@@ -0,0 +1,65 @@
+let userNum1 
+let userNum2 
+let userOperator
+let total = 0
+
+function toNumber(){
+    if (userNum1 || userNum2 == typeof 'string'){
+        userNum1 = Number(userNum1); 
+        userNum2 = Number(userNum2);
+    }
+}
+
+function add(){
+    total = userNum1 + userNum2
+    console.log(total)
+    return total
+}
+
+function sub(){
+    total = userNum1 - userNum2
+    console.log(total)
+    return total
+}
+
+function multi(){
+    total = userNum1 * userNum2
+    console.log(total)
+    return total
+}
+
+function divide(){
+    total = userNum1 / userNum2
+    console.log(total)
+    return total
+}
+
+function operate(userOperator){
+    switch(userOperator){
+        case '+':
+            total = add()
+            break;
+        case '-':
+            total = sub()
+            break;
+        case '*':
+            total = multi()
+            break;
+        case '/':
+            total = divide()
+            break;
+    }
+}
+
+function testInput(){
+     userNum1 = prompt()
+     userOperator = prompt()
+     userNum2 = prompt()
+    
+    toNumber()
+    operate(userOperator)
+}
+
+const testbtn = document.getElementById('btn')
+testbtn.addEventListener('click', testInput)
+
