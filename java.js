825143ad7fa43e606ca2c5ff6fd1f11d40323cc5
ChesterOM <131749381+ChesterOM@users.noreply.github.com>
2023-06-03 16:00:36 +1200

    test branch

--- java.js
+++ java.js
@@ -1,65 +1,125 @@
-let userNum1;
-let userNum2; 
+let userNum;
 let userOperator;
-let total = 0;
+let total;
 
 function toNumber(){
-    if (userNum1 || userNum2 == typeof 'string'){
-        userNum1 = Number(userNum1); 
-        userNum2 = Number(userNum2);
-    }
-};
+   
+    userNum = Number(accumulatedValue);
+    console.log(typeof userNum)
+}
+
 
 function add(){
-    total = userNum1 + userNum2;
-    console.log(total);
+    total = parseInt(total) + userNum;
     return total;
 };
 
 function sub(){
-    total = userNum1 - userNum2;
-    console.log(total);
+    total = total - userNum;;
     return total;
 };
 
 function multi(){
-    total = userNum1 * userNum2;
-    console.log(total);
+    total = total * userNum;;
     return total;
 };
 
 function divide(){
-    total = userNum1 / userNum2;
-    console.log(total);
+    total = total / userNum;;
     return total;
 };
 
 function operate(userOperator){
+    userNum = total;
+    
     switch(userOperator){
         case '+':
-            total = add();
+             add();
             break;
         case '-':
-            total = sub();
+             sub();
             break;
         case '*':
-            total = multi();
+             multi();
             break;
         case '/':
-            total = divide();
+             divide();
+            break;
+        case '=':
+          displayTotal()  
             break;
     }
+  
+    accumulatedValue = total.toString();
+}
+ 
+let accumulatedValue = 0;
+
+function display(){
+   let changeDisplay = document.getElementById('displaynumber');
+    changeDisplay.innerText = '' + accumulatedValue;
 }
 
-function testInput(){
-     userNum1 = prompt();
-     userOperator = prompt();
-     userNum2 = prompt();
-    
-    toNumber();
-    operate(userOperator);
-};
+function displayTotal(){
+    accumulatedValue = total.toString(); 
+}
 
-const testbtn = document.getElementById('btn');
-testbtn.addEventListener('click', testInput);
+function userClickNumber(event) {
+    const buttonValue = parseInt(event.target.textContent);
+    accumulatedValue += buttonValue;
+    console.log(accumulatedValue);
+    display()
+    toNumber()
+    console.log(typeof userNum)
+    return accumulatedValue;
+}
+
+function userClickOperator(event) {
+    const buttonValue = event.target.textContent;
+    userOperator = buttonValue
+    display()
+    operate(userOperator)
+    // convert total to string then post in display and concat with next input
+    console.log(userOperator)
+    return userOperator
+}
+
+// select number then when operate is selected save the selected number to a userinput
+
+// button click on '=' deletes current displaynumber and replaces with total
+
+
+function clickNum(){
+    const testbtn = document.getElementsByClassName('num');
+
+    for (let i = 0; i < testbtn.length; i++){
+    testbtn[i].addEventListener('click', userClickNumber);
+}
+}
+
+function clickOperator(){
+    const testbtn = document.getElementsByClassName('op');
+
+    for (let i = 0; i < testbtn.length; i++){
+    testbtn[i].addEventListener('click', userClickOperator);
+}
+}
+clickOperator();
+clickNum();
+
+
+
+
+
+// function testInput(){
+//      userNum1 = prompt();
+//      userOperator = prompt();
+//      userNum2 = prompt();
+    
+//     toNumber();
+//     operate(userOperator);
+// };
+
+// const testbtn = document.getElementById('btn');
+// testbtn.addEventListener('click', testInput);
 
